41741165718e005828059fc8adb05a7d67d58989
Tim Stewart <tuf19822@temple.edu>
2018-02-28 00:04:26 -0500

    Finished Commenting

--- Lab4ColorPallete/app/src/main/java/edu/temple/lab4colorpallete/ColorAdapter.java
+++ Lab4ColorPallete/app/src/main/java/edu/temple/lab4colorpallete/ColorAdapter.java
@@ -13,13 +13,17 @@ import android.widget.TextView;
 
 public class ColorAdapter extends BaseAdapter {
     private final Context mContext;
+
+    //the String array that will store our colors
     private final String[] colors;
 
+    //constructor
     public ColorAdapter(Context context, String[] colors){
         this.mContext = context;
         this.colors = colors;
     }
 
+    //required overrides
     @Override
     public int getCount(){
         return colors.length;
@@ -35,11 +39,19 @@ public class ColorAdapter extends BaseAdapter {
         return null;
     }
 
+    //this is called on each Item to determine what view to show in the GridView
     @Override
     public View getView(int position, View convertView, ViewGroup parent){
+        //creating a TextView object
         TextView colorTextView = new TextView(mContext);
+
+        //Setting the text to be the name of a color using the position arg
         colorTextView.setText(colors[position]);
+
+        //setting the background color of the textview to be the color specified by the name
         colorTextView.setBackgroundColor(Color.parseColor(colors[position]));
+
+        //returning the view we made
         return colorTextView;
     }
 }
--- Lab4ColorPallete/app/src/main/java/edu/temple/lab4colorpallete/PalleteActivity.java
+++ Lab4ColorPallete/app/src/main/java/edu/temple/lab4colorpallete/PalleteActivity.java
@@ -16,16 +16,29 @@ public class PalleteActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pallete);
 
+        //the list of color values
         final String[] colors = {"red", "blue", "green","black", "white", "gray", "cyan", "magenta", "yellow", "olive"};
 
+        //creating our gridview
         GridView gridview = (GridView) findViewById(R.id.gridview);
+
+        //setting the gridview adapter to be an instance of our custom ColorAdapter
         gridview.setAdapter(new ColorAdapter(this, colors));
 
+
+        //Creating a callback for when each of the gridview Items are clicked
         gridview.setOnItemClickListener(new OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+
+                //create an Intent Object, passing it our context and a class to identify the
+                //activity it should go to
                 Intent intent = new Intent(getApplicationContext(), ColorActivity.class);
+
+                //attaching a color String to our Intent using the position argument to the callback
                 intent.putExtra("color",colors[position]);
+
+                //starting a new activity with the intent we just made
                 startActivity(intent);
 
             }
