97ccdeba765ec8ab7eb1f5f76f476baf4fad1251
Tim Stewart <timothyforthewin@gmail.com>
2018-04-11 08:39:38 -0400

    Lab 8

--- Lab08SimpleBrowser/app/src/main/java/edu/temple/lab08simplebrowser/MainActivity.java
+++ Lab08SimpleBrowser/app/src/main/java/edu/temple/lab08simplebrowser/MainActivity.java
@@ -3,6 +3,8 @@ package edu.temple.lab08simplebrowser;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,6 +25,10 @@ public class MainActivity extends AppCompatActivity {
     WebTabPagerAdapter mWebTabPagerAdapter;
     //our current tab index
     int currentTab;
+    //the toolbar
+    Toolbar mToolbar;
+    //the current fragment
+    WebTabFragment currWebTabFragment;
 
 
     @Override
@@ -37,6 +43,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 //navigate the current tab to the URL currently in the EditText
                 String url = urlEditText.getText().toString();
+                currWebTabFragment = (WebTabFragment) mWebTabPagerAdapter.getItem(currentTab);
+                currWebTabFragment.navigateTo(url);
             }
         });
         // get a reference to the EditText
@@ -47,10 +55,44 @@ public class MainActivity extends AppCompatActivity {
         mWebTabPagerAdapter = new WebTabPagerAdapter(getSupportFragmentManager());
         //set our Pager's adapter
         mViewPager.setAdapter(mWebTabPagerAdapter);
+
+        mToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(mToolbar);
     }
 
     //the method we implement per the interface provided by WebTabFragment
     public void onUrlChanged(String url) {
         urlEditText.setText(url);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.action_backward:
+                currentTab --;
+                if (currentTab < 0){
+                    currentTab = 0;
+                }
+                updateFragment();
+                return true;
+            case R.id.action_forward:
+                currentTab ++;
+                if (currentTab > (mWebTabPagerAdapter.getCount() - 1)){
+                    currentTab = mWebTabPagerAdapter.getCount()-1;
+                }
+                updateFragment();
+                return true;
+            case R.id.action_new:
+                mWebTabPagerAdapter.newTab("https://www.google.com");
+                currentTab = mWebTabPagerAdapter.getCount()-1;
+                updateFragment();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void updateFragment(){
+        mViewPager.setCurrentItem(currentTab);
     }
 }
