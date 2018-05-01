ed537552dc851bec91d1ecfd62202e5dbbf61582
Tim Stewart <timothyforthewin@gmail.com>
2018-04-30 23:16:30 -0400

    Added checking symbol validty to Activity, and cleaned up the service

--- Lab09StockPortfolio/app/src/main/java/edu/temple/lab09stockportfolio/StockListActivity.java
+++ Lab09StockPortfolio/app/src/main/java/edu/temple/lab09stockportfolio/StockListActivity.java
@@ -18,8 +18,20 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.app.AlertDialog;
 
-import edu.temple.lab09stockportfolio.stock.StockContent;
+import org.json.JSONObject;
 
+import edu.temple.lab09stockportfolio.stock.StockContent;
+import edu.temple.lab09stockportfolio.stock.StockPollService;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 
 /**
@@ -32,6 +44,7 @@ public class StockListActivity extends AppCompatActivity {
      */
     private boolean mTwoPane;
     private String currSearchSymbol;
+    FileWriter portfolio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,6 +94,46 @@ public class StockListActivity extends AppCompatActivity {
         View recyclerView = findViewById(R.id.stock_list);
         assert recyclerView != null;
         setupRecyclerView((RecyclerView) recyclerView);
+
+        // check to see if the portfolio file exists, make it if it doesn't
+        String files[] = fileList();
+        if (files.length == 0 ){
+            File file = new File(StockListActivity.this.getFilesDir(), "portfolio.txt");
+        }
+
+        // If it's already running this will just get queue'd up
+        Intent mServiceIntent = new Intent(StockListActivity.this, StockPollService.class);
+        mServiceIntent.setAction(StockPollService.ACTION_START_POLLING);
+        StockListActivity.this.startService(mServiceIntent);
+    }
+
+    /**
+     * Here we check if a symbol is valid. If it is we write it to the file, then add it to
+     * a data structure? If not we'll display an invalid toast or something.
+     */
+    private void checkSymbolValidity(String symbol){
+        try {
+            URL queryURL = new URL("http://dev.markitondemand.com/MODApis/Api/v2/Quote/json/?" +
+                    "symbol=" + currSearchSymbol);
+            BufferedReader in = new BufferedReader(new InputStreamReader(queryURL.openStream()));
+            String line;
+            StringBuilder sb = new StringBuilder();
+            while ((line = in.readLine()) != null){
+                sb.append(line);
+            }
+            in.close();
+            JSONObject json = new JSONObject(sb.toString());
+            if (json.has("Message")){
+                // invalid stock
+                return;
+            } else {
+                // append the stock info to the file
+                portfolio = new FileWriter("portfolio.txt", true);
+                portfolio.write(currSearchSymbol + " " + json.get("Name") + " " + json.get("LastPrice"));
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
--- Lab09StockPortfolio/app/src/main/java/edu/temple/lab09stockportfolio/stock/StockPollService.java
+++ Lab09StockPortfolio/app/src/main/java/edu/temple/lab09stockportfolio/stock/StockPollService.java
@@ -5,15 +5,17 @@ import android.content.Intent;
 import android.content.Context;
 import android.os.SystemClock;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
 
 /**
  * A StockPollingService, that repeatedly checks the current prices of stocks listed in a file
  */
 public class StockPollService extends IntentService {
     // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
-    private static final String ACTION_START_POLLING = "edu.temple.lab09stockportfolio.stock.action.START_POLLING";
-
-    private static final String EXTRA_SYMBOL = "edu.temple.lab09stockportfolio.stock.extra.SYMBOL";
+    public static final String ACTION_START_POLLING = "edu.temple.lab09stockportfolio.stock.action.START_POLLING";
 
     public StockPollService() {
         super("StockPollService");
@@ -24,8 +26,7 @@ public class StockPollService extends IntentService {
         if (intent != null) {
             final String action = intent.getAction();
             if (ACTION_START_POLLING.equals(action)) {
-                final String symbol = intent.getStringExtra(EXTRA_SYMBOL);
-                handleActionStartPolling(symbol);
+                handleActionStartPolling();
             }
         }
     }
@@ -34,7 +35,21 @@ public class StockPollService extends IntentService {
      * Here we start polling. In a loop, we wait a minute, read through the file,
      * check the price of each symbol listed, and update them in the file
      */
-    private void handleActionStartPolling(String symbol) {
+    private void handleActionStartPolling() {
+        // open reading and writing streams to the portfolio file.
+        FileOutputStream portfolioOut;
+        try {
+            portfolioOut = openFileOutput("portfolio.txt", Context.MODE_PRIVATE);
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+        FileInputStream portfolioIn;
+        try {
+            portfolioIn = openFileInput("portfolio.txt");
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+
         while(true){
             // sleep for a minute
             SystemClock.sleep(1 * 60 * 1000);
